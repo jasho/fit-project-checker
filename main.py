f4d36c3fc41b77cf0e7e2fe92a7e213e49c406bf
HoundThe <cgkajm@gmail.com>
2021-01-31 18:11:07 +0100

    add tar support

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import argparse
 from zipfile import ZipFile
 import gzip
-import tarfile
+from tarfile import TarFile
 import os
 import sys
 
@@ -19,7 +19,7 @@ TODO list:
 
 """
 Supported archives/compression -
-.zip, .gz, .tar, .tar.gz
+.zip, .gz, .tar, .tar.gz, .tgz
 """
 
 # Right now hardcoded archive contents
@@ -29,7 +29,7 @@ archive_contents = [
 ]
 
 
-def check_zip(zip_name: str):
+def check_zip(zip_name: str, archive_contents: list):
     # Raises if file isn't valid ZipFile
     with ZipFile(os.path.join(os.path.abspath('.'), zip_name), 'r') as zip:
         file_list = zip.namelist()
@@ -37,22 +37,34 @@ def check_zip(zip_name: str):
             print(f"{zip_name} checks out")
 
 
-def check_archive(archive_name: str):
+def check_tar(zip_name: str, archive_contents: list, compression: bool):
+    if compression:
+        mode = 'r:gz'  # Open for reading with gzip compression.
+    else:
+        mode = 'r:'  # Open for reading exclusively without compression.
+
+    # Raises if it's not valid tar or valid compression
+    with TarFile.open(os.path.join(os.path.abspath('.'), zip_name), mode) as tar:
+        file_list = tar.getnames()
+        if (sorted(archive_contents) == sorted(file_list)):
+            print(f"{zip_name} checks out")
+
+
+def check_archive(archive_name: str, archive_contents: list):
     """
     Checks the archive format, compression, contents
     """
-    name, ext = archive_name.split('.', maxsplit=2)
+    name, ext = archive_name.split('.', maxsplit=1)
 
     # deduct archive type/compression from extension
     if ext == 'zip':
-        check_zip(archive_name)
+        check_zip(archive_name, archive_contents)
     elif ext == 'tar':
-        pass
+        check_tar(archive_name, archive_contents, False)
     elif ext == 'gz':
         pass
-    # probably useless extra branch as tar and tar.gz can be both handled by tarfile lib
-    elif ext == 'tar.gz':
-        pass
+    elif ext == 'tar.gz' or ext == 'tgz':
+        check_tar(archive_name, archive_contents, True)
     else:
         raise Exception("Unsupported archive/compression extension")
 
@@ -76,4 +88,4 @@ if __name__ == "__main__":
     if args.archive not in files:
         print('Archive is not in the cwd', file=sys.stderr)
 
-    check_archive(args.archive)
+    check_archive(args.archive, archive_contents)
